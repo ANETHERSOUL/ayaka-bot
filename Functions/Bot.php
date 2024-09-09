fc8618148e96213e4f6169ba2c527879f694a3b3
ANETHERSOUL <aon.campo.2003@gmail.com>
2024-09-09 15:39:05 -0700

    add bot funtion

--- Functions/Bot.php
+++ Functions/Bot.php
@@ -20,8 +20,41 @@ function Articmi($method, $data = [])
 function send_message($chat_id, $text, $keyboard)
 {
     Articmi('sendMessage', [
+        'chat_id'  =>  $chat_id,
+        'text'  =>  $text,
+        'reply_markup'  =>  $keyboard
+    ]);
+}
+
+function edit_message($chat_id, $message_id, $text, $reply_markup){
+	Articmi('editMessageText',[
+	    'chat_id' => $chat_id,
+	    'message_id' => $message_id,
+	    'text' => $text,
+	    'reply_markup' => $reply_markup
+    ]);
+}
+
+function forward_message($chatid, $from_chat_id, $message_id){
+	Articmi('forwardMessage',[
+        'chat_id' => $chatid,
+        'from_chat_id' => $from_chat_id,
+        'message_id' => $message_id
+    ]);
+}
+
+function copy_message($chatid, $from_chat_id, $message_id){
+    Articmi('copyMessage',[
+        'chat_id' => $chatid,
+        'from_chat_id' => $from_chat_id,
+        'message_id' => $message_id
+    ]);
+}
+
+function send_photo($chat_id, $photo, $keyboard){
+	Articmi('sendPhoto',[
         'chat_id' => $chat_id,
-        'text' => $text,
+        'photo' => $photo,
         'reply_markup' => $keyboard
     ]);
 }
